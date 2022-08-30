828a363f826a62d8d71d65ea1c51be94d15ffb7c
Alvaro <afalcon@triller.co>
2022-08-30 14:31:21 +0100

    wip: handling search

--- app/src/main/java/com/frostfel/animelist/views/season_list/SeasonAnimeFragment.kt
+++ app/src/main/java/com/frostfel/animelist/views/season_list/SeasonAnimeFragment.kt
@@ -1,10 +1,14 @@
 package com.frostfel.animelist.views.season_list
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -13,6 +17,7 @@ import com.frostfel.animelist.MainActivityViewModel
 import com.frostfel.animelist.databinding.SeasonAnimeFragmentBinding
 import com.frostfel.animelist.views.season_list.adapter.AnimeListAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -24,6 +29,8 @@ class SeasonAnimeFragment : Fragment() {
         activityViewModel.navigator.navigateToAnimeDetail(anime)
 
     }
+
+    private val handler = Handler(Looper.getMainLooper())
 
     companion object {
         fun newInstance() = SeasonAnimeFragment()
@@ -42,14 +49,39 @@ class SeasonAnimeFragment : Fragment() {
     private fun initView(binding: SeasonAnimeFragmentBinding) {
         binding.recylcerView.layoutManager = LinearLayoutManager(activity)
         binding.recylcerView.adapter = adapter
+        binding.searchView.setOnFocusChangeListener { _, focused ->
+            binding.searchView.gravity = when (focused) {
+                true -> {
+                    Gravity.START
+                }
+                else -> {
+                    Gravity.CENTER
+                }
+            }
+        }
+
+        binding.searchView.addTextChangedListener {
+            handler.removeCallbacksAndMessages(null)
+            handler.postDelayed({
+                it?.let {
+                    viewModel.filterText.postValue(it.trim().toString())
+                } ?: viewModel.filterText.postValue("")
+
+            }, 800)
+        }
     }
 
     private fun observeData() {
         lifecycleScope.launch {
-            viewModel.retrieveData().observe(viewLifecycleOwner) {
-                it?.let {
-                    adapter.submitData(lifecycle, it)
-                }
+            viewModel.filterText.observe(viewLifecycleOwner){
+                setTextQuery(it)
+            }
+        }
+    }
+    private fun setTextQuery(query: String) {
+        lifecycleScope.launch {
+            viewModel.retrieveData(query).collectLatest {
+                adapter.submitData(it)
             }
         }
     }
--- app/src/main/java/com/frostfel/animelist/views/season_list/SeasonAnimeViewModel.kt
+++ app/src/main/java/com/frostfel/animelist/views/season_list/SeasonAnimeViewModel.kt
@@ -1,22 +1,24 @@
 package com.frostfel.animelist.views.season_list
 
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import androidx.paging.filter
 import com.frostfel.animelist.model.Anime
 import com.frostfel.animelist.views.season_list.repository.AnimeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class SeasonAnimeViewModel @Inject constructor(
     private val animeRepository: AnimeRepository
 ) : ViewModel(), LifecycleObserver {
-
-    fun retrieveData(): LiveData<PagingData<Anime>> {
-        return animeRepository.getAnimeList().cachedIn(viewModelScope)
+    val filterText : MutableLiveData<String> = MutableLiveData("")
+    fun retrieveData(filter: String): Flow<PagingData<Anime>> {
+        return animeRepository.getAnimeList().asFlow()
+            .map { it.filter { item -> item.title.contains(filter, ignoreCase = true) } }
+            .cachedIn(viewModelScope)
     }
 }
